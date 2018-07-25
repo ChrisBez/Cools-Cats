e397e2dc06d156f1abb79291f56baf90b9b8b31b
ChrisBez <cjbezzina@gmail.com>
2018-07-25 20:59:48 +1000

    added httpClient module and added to service

--- cool-cats/src/app/app.module.ts
+++ cool-cats/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CatViewerComponent } from './cat-viewer/cat-viewer.component';
@@ -10,7 +11,8 @@ import { CatViewerComponent } from './cat-viewer/cat-viewer.component';
     CatViewerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
